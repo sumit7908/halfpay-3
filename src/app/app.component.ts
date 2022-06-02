cd006a271a3f3c5131bc9152d5b14fb33889e027
sumit7908 <sumitslgsmc@gmail.com>
2022-06-03 00:27:17 +0530

    latest modification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,56 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'add-to-cart';
- /*-----Google pay--------*/ 
 
-  paymentRequest:google.payments.api.PaymentDataRequest={
-    apiVersion: 2,
-    apiVersionMinor: 0,
-    allowedPaymentMethods:[
-      {
-        type: 'CARD',
-        parameters:{
-          allowedAuthMethods:['PAN_ONLY','CRYPTOGRAM_3DS'],
-          allowedCardNetworks:['AMEX','VISA','MASTERCARD']
-        },
-        tokenizationSpecification:{
-          type:'PAYMENT_GATEWAY',
-          parameters:{
-            gateway:'example',
-            gatewayMerchantId: 'examplegatewayMarchantId'
-          }
-        }
-
-      }
-    ],
-    merchantInfo:{
-      merchantId:'12345678901234567890',
-      merchantName:'Demo Merchant'
-    },
-    transactionInfo: {
-      totalPriceStatus: 'FINAL',
-      totalPriceLabel: 'Total',
-      totalPrice: '0.10',
-      currencyCode: 'INR',
-      countryCode: 'BE'
-    },
-    callbackIntents: ['PAYMENT_AUTHORIZATION']
-  };
-  onLoadPaymentData = (
-    event: Event
-  ): void => {
-    const eventDetail = event as CustomEvent<google.payments.api.PaymentData>;
-    console.log('load payment data', eventDetail.detail);
-  }
-  onPaymentDataAuthorized: google.payments.api.PaymentAuthorizedHandler = (
-    paymentData
-    ) => {
-      console.log('payment authorized', paymentData);
-      return {
-        transactionState: 'SUCCESS'
-      };
-    }
-    onError = (event: ErrorEvent): void => {
-      console.error('error', event.error);
-    }
 }
