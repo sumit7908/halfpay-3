85fc76f6705d378a2b4779667db7d9ad669d1a9a
sumit7908 <sumitslgsmc@gmail.com>
2022-06-11 22:42:57 +0530

    mod

--- src/app/component/prod-details/prod-details.component.ts
+++ src/app/component/prod-details/prod-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/service/product.service';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-prod-details',
@@ -11,17 +13,24 @@ export class ProdDetailsComponent implements OnInit {
 
   
 
-  constructor( public router : ActivatedRoute, private productService : ProductService) { }
+  constructor( public router : ActivatedRoute, private productService : ProductService,private api : ApiService, private cartService : CartService) { }
 
   id = this.router.snapshot.params['prod-id'];
   productDetails: any = {};
+  searchKey:string ="";
   ngOnInit(): void {
 
     this.productService.singleProduct(this.id)
     .subscribe((data : {} ) => {
       this.productDetails = data;
     })
+    this.cartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
+    })
 
+  }
+  addtocart(item: any){
+    this.cartService.addtoCart(item);
   }
 
 }
